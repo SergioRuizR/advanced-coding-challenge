4d395f49c7b2459a2801004b4dabc7039ebaf1a6
Saul Trujillo <Saul.Trujillo@Honeywell.com>
2022-11-04 14:05:02 -0600

    Add API Service application

---
+++ API/DaBulllet.TODO.API/Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DaBulllet.TODO.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
